4f3b5e00ef15c2f795e7e5a51f97aa2087059868
stepan-romankov <1895302+stepan-romankov@users.noreply.github.com>
2026-03-26 10:23:04 +0000

    Release 1.1.9

--- csharp/starter/T0.ProviderStarter/TemplateFiles.cs
+++ csharp/starter/T0.ProviderStarter/TemplateFiles.cs
@@ -63,7 +63,7 @@ public static class TemplateFiles
           </PropertyGroup>
 
           <ItemGroup>
-            <PackageReference Include="T0.ProviderSdk" Version="1.1.8" />
+            <PackageReference Include="T0.ProviderSdk" Version="1.1.9" />
             <PackageReference Include="DotNetEnv" Version="3.1.1" />
           </ItemGroup>
 
