b918e34be2f5f8f03fa8e22374c1fd2421f68a62
shimmeg <23666028+shimmeg@users.noreply.github.com>
2026-04-08 12:07:52 +0000

    Release 1.1.12

--- csharp/starter/T0.ProviderStarter/TemplateFiles.cs
+++ csharp/starter/T0.ProviderStarter/TemplateFiles.cs
@@ -63,7 +63,7 @@ public static class TemplateFiles
           </PropertyGroup>
 
           <ItemGroup>
-            <PackageReference Include="T0.ProviderSdk" Version="1.1.11" />
+            <PackageReference Include="T0.ProviderSdk" Version="1.1.12" />
             <PackageReference Include="DotNetEnv" Version="3.1.1" />
           </ItemGroup>
 
